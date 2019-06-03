2c3fa45ddef9c82dd251c1264aad0dde8dd98ee8
Esquilo <vinicius.borba@hotmail.com>
2019-06-03 18:50:11 -0300

    commit do react

---
+++ meu-aplicativo/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Clock from './clcock';
+import Toggle from './toggle';
+import LoginControl from './loginControl';
+import * as serviceWorker from './serviceWorker';
+//import { userInfo } from 'os';
+
+
+// ReactDOM.render(
+//     <Clock />,
+//     document.getElementById('root')
+// );
+
+
+  
+// ReactDOM.render(
+//     <Toggle />,
+//     document.getElementById('root')
+// );
+
+// ReactDOM.render(
+//     <LoginControl />,
+//     document.getElementById('root')
+// )
+
+
+serviceWorker.unregister();
