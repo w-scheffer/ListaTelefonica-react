5b8b1ca0ebb301518fb323570248fe81f0b634e3
Vinicius de Borba <vinicius.borba@softdesign.com.br>
2019-06-10 14:40:33 -0300

    foi

--- lista-telefonica/src/App.js
+++ lista-telefonica/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button'
+
 
 function App() {
   return (
@@ -10,15 +12,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <Button variant="light"
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </Button>
       </header>
+
     </div>
   );
 }
